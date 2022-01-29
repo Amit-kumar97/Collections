756cf2ec55693aad9c87c161144da0c8da958567
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-01-29 22:45:42 +0530

    Create simple.java

---
+++ src/ArrayLists/simple.java
@@ -0,0 +1,30 @@
+package ArrayLists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class simple {
+    public static void main(String[] args) {
+
+        List list = new ArrayList();
+        list.add(new String[]{"This", "is"});
+        list.add(new String[]{"a", "test."});
+
+        for (int i = 0; i < list.size(); i++)
+        {
+            String[] element = (String[]) list.get(i);
+
+            boolean space = false;
+            for (int j = 0; j < element.length; j++)
+            {
+                if (space)
+                {
+                    System.out.print(" ");
+                }
+                System.out.print(element[j]);
+                space = true;
+            }
+            System.out.print(" ");
+        }
+    }
+}
