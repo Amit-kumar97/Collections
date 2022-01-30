f838fc3a0591915b2101cc9bc450e139a503182a
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-01-30 23:17:46 +0530

    Update ArrayList_Operation.java

--- src/ArrayLists/ArrayList_Operation.java
+++ src/ArrayLists/ArrayList_Operation.java
@@ -37,6 +37,6 @@ public class ArrayList_Operation
         obj.insert(al);
         obj.delete(al);
         obj.update(al);
-        al.clear();
+        al.clear();                                 //-----------------------for deleting whole Arraylist
     }
 }
