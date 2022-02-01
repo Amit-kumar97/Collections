f11bc5ecc820f32f3519c983b2cc9a03a4add6aa
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-02-01 22:39:15 +0530

    Update linkedlist00.java

--- linkedlist00.java
+++ linkedlist00.java
@@ -1,19 +1,19 @@
 import java.util.LinkedList;
 import java.util.ListIterator;
- 
-public class linkedlist {
+
+public class LinkedList00 {
     public static void main(String args[]) {
- 
-     //---------Linked List Declaration
-     LinkedList<String> l_list = new LinkedList<String>();
-     //-----add(String Item) is used for adding the Items to the linked list
+
+        //---------Linked List Declaration
+        LinkedList<String> l_list = new LinkedList<String>();
+        //-----add(String Item) is used for adding the Items to the linked list
         l_list.add("India");
         l_list.add("Japan");
         l_list.add("China");
         l_list.add("South Korea");
-      System.out.println("Linked List Content: " +l_list);
-     
-      //---------Add Items at specified position
+        System.out.println("Linked List Content: " +l_list);
+
+        //---------Add Items at specified position
         l_list.add(2, "Russia");
         l_list.add(3, "Phillipins");
         System.out.println("L_List Content after Addition at specific index: " +l_list);
@@ -22,5 +22,28 @@ public class linkedlist {
         l_list.addFirst("First Country");
         l_list.addLast("Last Country");
         System.out.println("L_List Content after addition at first and last: " +l_list);
+
+        //----------Get and set Items in the list
+        Object firstvar = l_list.get(0);
+        System.out.println("First Item: " +firstvar);
+        l_list.set(0, "America");
+        System.out.println("L_List Content after updating first Item: " +l_list);
+
+        //----------Remove from a position
+        l_list.remove(1);
+        l_list.remove(2);
+        System.out.println("LinkedList after deletion of Item in 2nd and 3rd position " +l_list);
+
+        //----------Remove first and last Item
+        l_list.removeFirst();
+        l_list.removeLast();
+        System.out.println("Final Content after removing first and last Item: "+l_list);
+
+        //----------Iterating the linked list
+        ListIterator<String> itrator = l_list.listIterator();
+        System.out.println("List displayed using iterator:");
+        while (itrator.hasNext()) {
+            System.out.println(itrator.next());
+        }
     }
 }
