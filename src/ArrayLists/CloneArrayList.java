440ea69bccd0b540eef02fb9e5d25b11b87a6fb8
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-02-06 23:54:13 +0530

    Update CloneArrayList.java

--- src/ArrayLists/CloneArrayList.java
+++ src/ArrayLists/CloneArrayList.java
@@ -8,13 +8,13 @@ Write a Java program to clone an array list to another array list.
 public class CloneArrayList {
 
     public static void main(String[] args) {
-        
+
         ArrayList<String> original = new ArrayList<String>();
-        c1.add("Red");
-        c1.add("Green");
-        c1.add("Black");
-        c1.add("White");
-        c1.add("Pink");
+        original.add("Red");
+        original.add("Green");
+        original.add("Black");
+        original.add("White");
+        original.add("Pink");
         System.err.println("Original array list: " + original);
 
         ArrayList<String> newclone = (ArrayList<String>)original.clone();                //-----------CLONING ORINGINAL ARRAYLIST
