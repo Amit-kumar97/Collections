7f795fc21662212fd7e6fb9913d4a2cc9e70d413
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-02-08 23:21:30 +0530

    Update linkedlist01.java
    simple representation of linked list.(Collection)

--- linkedlist01.java
+++ linkedlist01.java
@@ -1,31 +1,40 @@
 package LinkedLists;
 
+//----------Node class with Generics
 class Node<T>
 {
     T data;
     Node<T> next;
+    //--------Constructor
+
     Node(T data)
     {
         this.data = data;
     }
 }
-public class linkedlist01 
+//----------definition class
+public class linkedlist01
 {
+    //--------method to print the Node values
     public static void print(Node<Integer> head)
     {
-        Node<Integer> temp = head;
+        Node<Integer> temp = head;              //---------creating a temporary Node to hold reference of head
 
-        while(temp!=null)
-        {
+        while(temp!=null)                       //---------it will print the values until head.next is not equals to null
+        {                                       //---------if condition gets fail than it will come out from the loop
             System.out.print(temp.data+" ");
-            temp = temp.next;
+            temp = temp.next;                    //---------move head next to their corresponding Node
         }
         System.out.println();
     }
+    //----------main method in which we initialises the Linked List
     public static void main(String[] args) {
+
         Node<Integer> node1 = new Node<Integer>(10);
         Node<Integer> node2 = new Node<Integer>(20);
-        node1.next = node2;
+        Node<Integer> node3 = new Node<Integer>(30);
+        node1.next = node2;                                 //-----------passing the reference of 2nd Node to their previous one
+        node2.next = node3;
         print(node1);
     }
 }
